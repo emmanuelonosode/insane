ff0a53c690e75ccf4ccc1b511b343d0aafe79f11
emmanuel onosode <emmanuelonosode4@gmail.com>
2025-08-24 10:10:01 +0100

    Remove unused Cloudflare configuration files and update author schema with additional social media fields.

--- schemaTypes/author.js
+++ schemaTypes/author.js
@@ -1,36 +1,66 @@
 export default {
-  name: "author",
-  title: "Author",
-  type: "document",
+  name: 'author',
+  title: 'Author',
+  type: 'document',
   fields: [
     {
-      name: "name",
-      title: "Name",
-      type: "string",
+      name: 'name',
+      title: 'Name',
+      type: 'string',
       validation: (Rule) => Rule.required(),
     },
     {
-      name: "slug",
-      title: "Slug",
-      type: "slug",
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
       options: {
-        source: "name",
+        source: 'name',
         maxLength: 96,
       },
       validation: (Rule) => Rule.required(),
     },
     {
-      name: "image",
-      title: "Image",
-      type: "image",
+      name: 'image',
+      title: 'Image',
+      type: 'image',
       options: {
         hotspot: true,
       },
     },
     {
-      name: "bio",
-      title: "Bio",
-      type: "text",
+      name: 'bio',
+      title: 'Bio',
+      type: 'text',
+    },
+    {
+      name: 'twitter',
+      title: 'Twitter',
+      type: 'text',
+    },
+    {
+      name: 'linkedin',
+      title: 'Linkedin',
+      type: 'text',
+    },
+    {
+      name: 'github',
+      title: 'Github',
+      type: 'text',
+    },
+    {
+      name: 'instagram',
+      title: 'Instagram',
+      type: 'text',
+    },
+    {
+      name: 'tiktok',
+      title: 'Tiktok',
+      type: 'text',
+    },
+    {
+      name: 'facebook',
+      title: 'Facebook',
+      type: 'text',
     },
   ],
-};
+}
